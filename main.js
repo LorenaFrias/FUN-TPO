43b488e3d490008b8d9ecfc5ea0ee5db16d4c25d
Lorena Frias <lorenavfrias@gmail.com>
2022-11-02 18:35:07 -0300

    Added IO

--- main.js
+++ main.js
@@ -33,6 +33,23 @@ function countClicks(){
 }
 
 
+// ANIMACION 
+
+const slides = document.querySelectorAll('.square')
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+        if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
+}, {
+    threshold: 0.3,
+    
+});
+
+slides.forEach(slide => {
+    observer.observe(slide)
+})
 
 // API REST
 
