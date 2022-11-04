74e0c1e2525b45de52f07b1761a882a8472ad09a
Lorena Frias <lorenavfrias@gmail.com>
2022-11-03 23:45:55 -0300

    Added scroll up button

--- main.js
+++ main.js
@@ -157,3 +157,15 @@ form.submit();
 const emailIsValid = email => {
 return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
+
+// SCROLL UP BUTTON
+
+const scrollBtn = document.querySelector('.scrollup');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = 'block';
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+})
